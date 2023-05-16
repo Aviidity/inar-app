65544ae329a0178111d2dd78703d050b18da618f
Clarence Palad Ocampo <clarenceocampo25@gmail.com>
2023-05-16 18:58:45 +0200

    User registration & authentication using JWT w FireBase alg HS256

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Firebase\JWT\JWT;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     public function signup(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required | email | unique:users',
+            'password' => 'required | min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
         $user = User::create([
             'name' => $request->input('name'),
             'email' => $request->input('email'),
@@ -16,6 +28,39 @@ class AuthController extends Controller
         ]);
         
         return response()->json(['message' => 'User signed up successfully'], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required | email',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        $user = User::where('email', $request->input('email'))->first();
+
+        if (!$user) {
+            return response()->json(['error' => 'Email is not registered'], 404);
+        }
+
+        if (!password_verify($request->input('password'), $user->password)) {
+            return response()->json(['error' => 'Incorrect password'], 400);
+        }
+
+        $payload = [
+            'iss' => config('app.url'),
+            'sub' => $user->id,
+            'iat' => time(),
+            'exp' => time() + (60 * 60 * 24),
+        ];
+
+        $jwt = JWT::encode($payload, config('app.key'), 'HS256');
+
+        return response()->json(['token' => $jwt]);
     }
     // Route::get('/user/{id}', [UserController::class, 'show']);
 
